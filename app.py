0f08f0d73de6d6dae07765e4c78413ad064806b7
Janak Singh <janaksinghhimt@gmail.com>
2026-07-24 05:46:11 +0000

    Upgrade Docker app with advanced production practices

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
+APP_MESSAGE = os.getenv("APP_MESSAGE", "Hello Janak from Docker!")
+
 @app.route("/")
 def home():
-    return "Hello Janak from Docker!"
+    return APP_MESSAGE
 
 app.run(host="0.0.0.0", port=5000)
